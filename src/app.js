239920660d031bb0caba4aa4ba2d526d29f3007a
yahya momin <yahyamomin06@gmail.com>
2024-10-29 16:15:40 +0530

    add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 import hiAnimeRoutes from "./routes/routes.js";
 
 const app = new Hono();
+
+app.use(
+   "*",
+   cors({
+      origin: "*", // Allow any origin
+      allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowHeaders: ["Content-Type", "Authorization"],
+   })
+);
 app.get("/", (c) => {
    c.status(200);
    return c.text("welcome to anime API 🎉 start by hitting /api/v1 for documentation");
