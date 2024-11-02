e3f26c6ae1ea008ab28b466139f7e873781f3ad4
yahya momin <yahyamomin06@gmail.com>
2024-11-02 16:58:01 +0530

    testing

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,4 +1,8 @@
-import { fetchFromApi, fetchSources, interceptor } from "../axiosInstances/interceptor.js";
+import {
+  fetchFromApi,
+  fetchSources,
+  interceptor,
+} from "../axiosInstances/interceptor.js";
 import { extractListPage } from "../extractor/list_page.js";
 import { extractHomePage } from "../extractor/home_page.js";
 import { extractInfoPage } from "../extractor/info_page.js";
@@ -15,326 +19,342 @@ import apiDocumentation from "../utils/documentation.js";
 import { extractEpisodesInChunks } from "../extractor/extractEpisodes.js";
 
 export const home = async (c) => {
-   try {
-      return c.json(apiDocumentation);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+  try {
+    return c.json(apiDocumentation);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getHomePage = async (c) => {
-   try {
-      const obj = await interceptor("/home");
+  try {
+    const obj = await interceptor("/home");
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractHomePage(obj.data);
+    const response = extractHomePage(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
 
-      return setError(c, 500, "something went wrong");
-   }
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getInfo = async (c) => {
-   try {
-      const id = c.req.param("id");
-      console.log(id);
-      const obj = await interceptor(`/${id}`);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
-      const response = extractInfoPage(obj.data);
+  try {
+    const id = c.req.param("id");
+    console.log(id);
+    const obj = await interceptor(`/${id}`);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
+    const response = extractInfoPage(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
 
-      return setError(c, 500, "something went wrong");
-   }
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getRecommendation = async (c) => {
-   try {
-      const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-      if (!id) return setError(c, 404, "id is required");
+    if (!id) return setError(c, 404, "id is required");
 
-      const obj = await interceptor(`/${id}`);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
-      const response = extractRecommendation(obj.data);
+    const obj = await interceptor(`/${id}`);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
+    const response = extractRecommendation(obj.data);
 
-      if (response.length < 1) return setError(c, 404, "page not found");
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (response.length < 1) return setError(c, 404, "page not found");
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getCharacters = async (c) => {
-   try {
-      const id = c.req.param("id");
-      const page = c.req.query("page") || 1;
+  try {
+    const id = c.req.param("id");
+    const page = c.req.query("page") || 1;
 
-      if (!id) return setError(c, 404, "id is required");
+    if (!id) return setError(c, 404, "id is required");
 
-      const idNum = id.split("-").pop();
-      const endpoint = `/ajax/character/list/${idNum}?page=${page}`;
-      const obj = await fetchFromApi(id, endpoint);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    const idNum = id.split("-").pop();
+    const endpoint = `/ajax/character/list/${idNum}?page=${page}`;
+    const obj = await fetchFromApi(id, endpoint);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractCharacters(obj.data);
+    const response = extractCharacters(obj.data);
 
-      if (response.length < 1) return setError(c, 404, "page not found");
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (response.length < 1) return setError(c, 404, "page not found");
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getCharacter_info = async (c) => {
-   try {
-      const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-      if (!id) return setError(c, 404, "id is required");
+    if (!id) return setError(c, 404, "id is required");
 
-      const obj = await interceptor(`/character/${id}`);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    const obj = await interceptor(`/character/${id}`);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractCharacterInfo(obj.data);
+    const response = extractCharacterInfo(obj.data);
 
-      if (response.length < 1) return setError(c, 404, "page not found");
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (response.length < 1) return setError(c, 404, "page not found");
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getActor_info = async (c) => {
-   try {
-      const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-      if (!id) return setError(c, 404, "id is required");
+    if (!id) return setError(c, 404, "id is required");
 
-      const obj = await interceptor(`/people/${id}`);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    const obj = await interceptor(`/people/${id}`);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractActor(obj.data);
+    const response = extractActor(obj.data);
 
-      if (response.length < 1) return setError(c, 404, "page not found");
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (response.length < 1) return setError(c, 404, "page not found");
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getRelated = async (c) => {
-   try {
-      const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-      if (!id) return setError(c, 404, "id is required");
+    if (!id) return setError(c, 404, "id is required");
 
-      const obj = await interceptor(`/${id}`);
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    const obj = await interceptor(`/${id}`);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractRelated(obj.data);
+    const response = extractRelated(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getListPage = async (c) => {
-   try {
-      const validateQueries = [
-         "top-airing",
-         "most-popular",
-         "most-favorite",
-         "completed",
-         "recently-added",
-         "recently-updated",
-         "top-upcoming",
-         "genre",
-         "az-list",
-         "subbed-anime",
-         "dubbed-anime",
-         "movie",
-         "tv",
-         "ova",
-         "ona",
-         "special",
-         "events",
-      ];
-      const query = c.req.param("query").toLowerCase() || null;
+  try {
+    const validateQueries = [
+      "top-airing",
+      "most-popular",
+      "most-favorite",
+      "completed",
+      "recently-added",
+      "recently-updated",
+      "top-upcoming",
+      "genre",
+      "az-list",
+      "subbed-anime",
+      "dubbed-anime",
+      "movie",
+      "tv",
+      "ova",
+      "ona",
+      "special",
+      "events",
+    ];
+    const query = c.req.param("query").toLowerCase() || null;
 
-      if (!validateQueries.includes(query)) return setError(c, 404, "invalid query");
+    if (!validateQueries.includes(query))
+      return setError(c, 404, "invalid query");
 
-      const category = c.req.param("category") || null;
-      const page = c.req.query("page") || 1;
-      const endpoint = category ? `/${query}/${category}?page=${page}` : `/${query}?page=${page}`;
+    const category = c.req.param("category") || null;
+    const page = c.req.query("page") || 1;
+    const endpoint = category
+      ? `/${query}/${category}?page=${page}`
+      : `/${query}?page=${page}`;
 
-      const obj = await interceptor(endpoint);
+    const obj = await interceptor(endpoint);
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
-      const response = extractListPage(obj.data);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
+    const response = extractListPage(obj.data);
 
-      if (response.response.length < 1) return setError(c, 404, "page not found");
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (response.response.length < 1) return setError(c, 404, "page not found");
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getSearchPage = async (c) => {
-   try {
-      const keyword = c.req.query("keyword") || null;
-      const page = c.req.query("page") || 1;
+  try {
+    const keyword = c.req.query("keyword") || null;
+    const page = c.req.query("page") || 1;
 
-      if (!keyword) return setError(c, 404, "query is required");
+    if (!keyword) return setError(c, 404, "query is required");
 
-      const noSpaceKeyword = keyword.trim().toLowerCase().replace(/\s+/g, "+");
+    const noSpaceKeyword = keyword.trim().toLowerCase().replace(/\s+/g, "+");
 
-      console.log(noSpaceKeyword);
+    console.log(noSpaceKeyword);
 
-      const endpoint = `/search?keyword=${noSpaceKeyword}&page=${page}`;
-      const obj = await interceptor(endpoint);
+    const endpoint = `/search?keyword=${noSpaceKeyword}&page=${page}`;
+    const obj = await interceptor(endpoint);
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const response = extractListPage(obj.data);
+    const response = extractListPage(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getEpisodes = async (c) => {
-   try {
-      const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-      if (!id) return setError(c, 400, "id is required");
+    if (!id) return setError(c, 400, "id is required");
 
-      const Referer = `/watch/${id}`;
+    const Referer = `/watch/${id}`;
 
-      const idNum = id.split("-").at(-1);
-      const obj = await fetchFromApi(Referer, `/ajax/v2/episode/list/${idNum}`);
+    const idNum = id.split("-").at(-1);
+    const obj = await fetchFromApi(Referer, `/ajax/v2/episode/list/${idNum}`);
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
-      const response = extractEpisodes(obj.data);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
+    const response = extractEpisodes(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getServers = async (c) => {
-   try {
-      const episodeId = c.req.query("episodeId");
+  try {
+    const episodeId = c.req.query("episodeId");
 
-      if (!episodeId) return setError(c, 400, "episodeId is required");
+    if (!episodeId) return setError(c, 400, "episodeId is required");
 
-      const episode = episodeId.split("ep=").at(-1);
+    const episode = episodeId.split("ep=").at(-1);
 
-      const obj = await fetchFromApi(episodeId, `/ajax/v2/episode/servers?episodeId=${episode}`);
+    const obj = await fetchFromApi(
+      episodeId,
+      `/ajax/v2/episode/servers?episodeId=${episode}`
+    );
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
-      const response = extractServers(obj.data);
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
+    const response = extractServers(obj.data);
 
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 export const getSources = async (c) => {
-   try {
-      const { episodeId, server = 4, audio = "sub" } = c.req.query();
+  try {
+    const { episodeId, server = 4, audio = "sub" } = c.req.query();
 
-      console.log(episodeId, server, audio);
+    console.log(episodeId, server, audio);
 
-      const validServerIndexes = [4, 1];
+    const validServerIndexes = [4, 1];
 
-      const integerIndex = Number(server);
+    const integerIndex = Number(server);
 
-      if (!validServerIndexes.includes(integerIndex)) return setError(c, 400, "invalid server");
+    if (!validServerIndexes.includes(integerIndex))
+      return setError(c, 400, "invalid server");
 
-      if (!episodeId) return setError(c, 400, "episodeId is required");
+    if (!episodeId) return setError(c, 400, "episodeId is required");
 
-      const episode = episodeId.includes("ep=");
-      if (!episode) return setError(c, 400, "episode  is required");
-      const serverIdsHTML = await fetchFromApi(
-         episodeId,
-         `/ajax/v2/episode/servers?episodeId=${episodeId.split("ep=").at(-1)}`
-      );
+    const episode = episodeId.includes("ep=");
+    if (!episode) return setError(c, 400, "episode  is required");
+    const serverIdsHTML = await fetchFromApi(
+      episodeId,
+      `/ajax/v2/episode/servers?episodeId=${episodeId.split("ep=").at(-1)}`
+    );
 
-      const serverIds = extractServers(serverIdsHTML.data);
+    const serverIds = extractServers(serverIdsHTML.data);
 
-      const selectedServer = serverIds[audio].find((el) => el.index === integerIndex);
+    const selectedServer = serverIds[audio].find(
+      (el) => el.index === integerIndex
+    );
 
-      const obj = await fetchSources(episodeId, `/ajax/v2/episode/sources?id=${selectedServer.id}`);
+    const obj = await fetchSources(
+      episodeId,
+      `/ajax/v2/episode/sources?id=${selectedServer.id}`
+    );
 
-      if (!obj.status) {
-         return setError(c, 400, "make sure given endpoint is correct");
-      }
+    if (!obj.status) {
+      return setError(c, 400, "make sure given endpoint is correct");
+    }
 
-      const params = {
-         data: obj.data,
-         audio,
-         episodeId,
-         server,
-      };
-      const response = await extractSource(params);
-      return setResponse(c, 200, response);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    const params = {
+      data: obj.data,
+      audio,
+      episodeId,
+      server,
+    };
+    const response = await extractSource(params);
+    return setResponse(c, 200, response);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
 
 export const getEpisodesInChunks = async (c) => {
-   try {
-      const id = c.req.param("id");
-      if (!id) return setError(c, 400, "id is required");
+  try {
+    const id = c.req.param("id");
+    if (!id) return setError(c, 400, "id is required");
 
-      const obj = await interceptor(`/${id}`);
-      if (!obj.status) return setError(c, 400, "make sure the id is correct");
+    const obj = await interceptor(`/${id}`);
+    if (!obj.status) return setError(c, 400, "make sure the id is correct");
 
-      const response = await extractEpisodesInChunks(obj.data);
+    const response = await extractEpisodesInChunks(obj.data);
 
-      if (!response.status)
-         return setError(c, 400, "some thing went wrong while fetching chunks episodes");
-      return setResponse(c, 200, response.data);
-   } catch (error) {
-      console.log(error.message);
-      return setError(c, 500, "something went wrong");
-   }
+    if (!response.status)
+      return setError(
+        c,
+        400,
+        "some thing went wrong while fetching chunks episodes"
+      );
+    return setResponse(c, 200, response.data);
+  } catch (error) {
+    console.log(error.message);
+    return setError(c, 500, "something went wrong");
+  }
 };
