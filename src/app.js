a58f3683afd1695a6af8b2f9f73230b22e01083e
yahya momin <yahyamomin06@gmail.com>
2025-01-06 12:41:39 +0530

    add env file

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { config } from 'dotenv'
 
-import hiAnimeRoutes from "./routes/routes.js";
+import hiAnimeRoutes from './routes/routes.js'
 
-const app = new Hono();
+const app = new Hono()
+
+config()
+const origins = process.env.ORIGIN.split(',')
+console.log(origins)
 
 app.use(
-  "*",
+  '*',
   cors({
-    origin: "*", // Allow any origin
-    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowHeaders: ["Content-Type", "Authorization"],
+    origin: origins,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
   })
-);
-app.get("/", (c) => {
-  c.status(200);
-  return c.text(
-    "welcome to anime API 🎉 start by hitting /api/v1 for documentation"
-  );
-});
-app.get("/test", (c) => {
+)
+app.get('/', (c) => {
+  c.status(200)
+  return c.text('welcome to anime API 🎉 start by hitting /api/v1 for documentation')
+})
+app.get('/test', (c) => {
   return c.json({
     status: true,
-  });
-});
-app.route("/api/v1", hiAnimeRoutes);
+  })
+})
+app.route('/api/v1', hiAnimeRoutes)
 
-export default app;
+export default app
